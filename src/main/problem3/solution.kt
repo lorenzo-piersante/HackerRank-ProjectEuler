2914fbf9e8bafaac6b5bde68cb6eeee5614c8dae
lorenzo-piersante <l.piersante0@gmail.com>
2022-07-23 12:41:28 +0200

    problem 3 first solution

--- src/main/problem3/solution.kt
+++ src/main/problem3/solution.kt
@@ -1,9 +1,27 @@
 package problem3
 
-import java.util.*
+import java.util.Scanner
 
-fun getMaxPrimeFactor(input : Long) : Int {
-    TODO()
+/**
+ * bruteforce solution with O(n) complexity
+ * apparently working, but fails one HackerRank test case due Time limit exceeded
+ */
+fun getMaxPrimeFactor(input : Long) : Long {
+    var n = input
+    var candidate = 1L
+
+    if (input < 2) {
+        return candidate
+    }
+
+    for (i in 2L..n) {
+        if (n % i == 0L) {
+            n /= i
+            if (i > candidate) candidate = i
+        }
+    }
+
+    return candidate
 }
 
 fun main() {
