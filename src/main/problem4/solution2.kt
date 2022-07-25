f08cdc23aadfda786b5634330bccbca7853096aa
lorenzo-piersante <l.piersante0@gmail.com>
2022-07-25 12:19:57 +0200

    bugfix and cleanup on solution 2

--- src/main/problem4/solution2.kt
+++ src/main/problem4/solution2.kt
@@ -2,21 +2,18 @@ package problem4
 
 import java.util.Scanner
 
-private fun isPalindrome(n : Int) : Boolean{
+private fun isPalindrome(n : Int) : Boolean {
     val straight = n.toString()
     val reversed = straight.reversed()
 
     return straight == reversed
 }
 
-private fun getNextPalindrome(n : Int) : Int {
-    if (isPalindrome(n)) return n
+private fun getPreviousPalindrome(n : Int) : Int {
+    var prev = n
+    while (! isPalindrome(prev)) prev--
 
-    for (i in n-1 downTo 101101) {
-        if (isPalindrome(i)) return i
-    }
-
-    return 101101
+    return prev
 }
 
 private fun is3DigitTermsProduct(n : Int) : Boolean {
@@ -32,17 +29,15 @@ private fun is3DigitTermsProduct(n : Int) : Boolean {
 /**
  * this solution is way better, in fact we use the nested loops only to find the product terms only for palindrome nums
  * the optimization seems to be enough to pass tests
- * anyway this solution is failing one testcase on HackerRank for Wrong Answer
- * I am trying to figure out where the bug is ...
  */
 fun largestPalindromeProductV2(input : Int) : Int {
-    var n = getNextPalindrome(input)
-    while (n > 101101) {
-        if (is3DigitTermsProduct(n)) return n
-        n = getNextPalindrome(n-1)
+    var candidate = getPreviousPalindrome(input - 1)
+
+    while (! is3DigitTermsProduct(candidate)) {
+        candidate = getPreviousPalindrome(candidate - 1)
     }
 
-    return 101101
+    return candidate
 }
 
 fun main() {
--- src/test/problem4/LargestPalindromeProduct.kt
+++ src/test/problem4/LargestPalindromeProduct.kt
@@ -9,12 +9,16 @@ class LargestPalindromeProduct {
     fun shouldSolveTheProblem() {
         Assertions.assertEquals(101101, largestPalindromeProduct(101110))
         Assertions.assertEquals(793397, largestPalindromeProduct(800000))
+        Assertions.assertEquals(906609, largestPalindromeProduct(1000000))
+        Assertions.assertEquals(99999, largestPalindromeProduct(101101))
     }
 
     @Test
     fun shouldSolveTheProblemV2() {
         Assertions.assertEquals(101101, largestPalindromeProductV2(101110))
         Assertions.assertEquals(793397, largestPalindromeProductV2(800000))
+        Assertions.assertEquals(906609, largestPalindromeProductV2(1000000))
+        Assertions.assertEquals(99999, largestPalindromeProductV2(101101))
     }
 
 }
